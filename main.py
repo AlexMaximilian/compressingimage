ec46f1c95ef0be2487ec221421ce25146a5d1805
Alex Maximilian <alexandervanpoecke1@gmail.com>
2023-01-10 14:10:54 +0000

    showing an image with pillow

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Shalom")
+import os 
+
+from PIL import Image
+
+# open method used to open different extension image file
+im = Image.open("/Users/alex/OneDrive - Wellington College/Documents/cs/Alevel/compressingimage/hardwood-tree.jpeg") 
+  
+# This method will show image in any image viewer 
+im.show() 
