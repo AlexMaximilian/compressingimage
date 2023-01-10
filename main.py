d8a5da56cb624b9c3e84d2740c550a27fdbfa210
Alex Maximilian <alexandervanpoecke1@gmail.com>
2023-01-10 14:12:24 +0000

    commits

---
+++ main.py
@@ -0,0 +1,9 @@
+import os 
+
+from PIL import Image
+
+# open method used to open different extension image file
+im = Image.open("/Users/alex/OneDrive - Wellington College/Documents/cs/Alevel/compressingimage/hardwood-tree.jpeg") 
+  
+# This method will show image in any image viewer 
+im.show() 
